807195f5c35c72a77f610299034e71cc587004ad
micaelalay <micalay003@gmail.com>
2023-05-07 02:42:45 -0300

    added base for theme change

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
 const $ = (selector) => document.querySelector(selector)
 const $$ = (selector) => document.querySelectorAll(selector)
 
+// Variables
 
 const $hero = $(".hero")
 const $about = $(".about")
 const $projects = $(".projects")
 const $contact = $(".contact")
+
 const $$navLinks = $$(".nav-link")
+
+const $lightTheme = $(".theme__light")
+const $darkTheme = $(".theme__dark")
 
 const sections = [$hero, $about, $projects, $contact]
 
+const themes = [$lightTheme, $darkTheme]
+
+let currentTheme = localStorage.getItem("theme")
+
+// Functions
+
+const addActiveClass = (element) => element.classList.add("active")
+
+const removeActiveClass = (element) => element.classList.remove("active")
 
 const setActiveToNavBarLinks = () => {
     for (const section of sections) {
@@ -21,17 +35,45 @@ const setActiveToNavBarLinks = () => {
 
                 const navLinkName = navLink.getAttribute("name")
 
-                if (section.id === navLinkName) navLink.classList.add("active")
-                else navLink.classList.remove("active")
+                if (section.id === navLinkName) addActiveClass(navLink)
+                else removeActiveClass(navLink)
             }
         }
     }
 }
 
+const setCurrentTheme = (theme) => localStorage.setItem("theme", theme)
+
+const selectActiveTheme = (e) => {
+    for (const theme of themes) {
+        if (theme === e.target) {
+            addActiveClass(theme)
+            setCurrentTheme(theme.name)
+        }
+        else removeActiveClass(theme)
+    }
+}
+
+const setOnloadTheme = (currentTheme) => {
+    for (const theme of themes) {
+        theme.name === currentTheme && addActiveClass(theme)
+    }
+}
+
+// Events
+
 window.addEventListener("load", () => {
     setActiveToNavBarLinks()
+    setCurrentTheme(currentTheme)
+    setOnloadTheme(currentTheme)
 })
 
 window.addEventListener("scroll", () => {
     setActiveToNavBarLinks()
 })
+
+for (const theme of themes) {
+    theme.addEventListener("click", (e) => {
+        selectActiveTheme(e)
+    })
+}
